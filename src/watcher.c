df4b23936e30e75d70ecb29b73a5febf90d4404e
RayAuxey <rayauxey@gmail.com>
2023-08-13 19:36:24 +0300

    Function declarations

--- src/watcher.c
+++ src/watcher.c
@@ -10,3 +10,11 @@
 #include <assert.h>
 #include <sys/stat.h>
 #include <systemd/sd-journal.h>
+bool check_extension(char *filename, const char *extension);
+char *get_filename_without_extension(char *filename);
+char *join_strs(char *str1, char *str2);
+int make_executable(const char *filename);
+bool check_if_service();
+void log_message(const char *message);
+void log_message_with_priority(const char *message, int priority);
+bool is_systemd_parent();
